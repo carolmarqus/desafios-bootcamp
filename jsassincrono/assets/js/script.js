4ccc82af9d8c76987c82770ee62381828d67df1f
carolmarqus <carolllinnemarquess@gmail.com>
2022-08-12 16:39:21 -0300

    tentando resolver o problema no desafio- sem sucesso

--- jsassincrono/assets/js/script.js
+++ jsassincrono/assets/js/script.js
@@ -1,19 +1,18 @@
 const urlApi = "https://thatcopy.pw/catapi/rest/";
-const botao = document.getElementById("muda-cat");
-
 const puxarCats = async () => {
   try {
     const dados = await fetch(urlApi);
     const json = await dados.json();
     return json.webpurl;
   } catch (error) {
-    console.log(error.message);
+    console.log(error.stack);
   }
 };
 const carregaCats = async () => {
-  const implementar = document.getElementById("cat-aqui");
-  implementar.src = await puxarCats();
+  const img = document.getElementsByTagName('img') [0];
+  img.src = await puxarCats();
 };
-botao.addEventListener("click", carregaCats);
-
 carregaCats();
+const btn = document.getElementById('muda-cat');
+btn.addEventListener('click', carregaCats);
+
